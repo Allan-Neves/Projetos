c39a602a02e3d6f144f9720c8fc58c4e3893b402
Allan Neves <allan3017carlos@gmail.com>
2024-05-22 17:42:03 -0300

    Adicionando novos projetos, e removendo descrições

--- assets/data.js
+++ assets/data.js
@@ -1,12 +1,57 @@
 var cardData = [
   {
+    imgSrc: "images/creta-chale.png",
+    imgAlt: "Creta-Chale",
+    title: "Creta-Chale",
+    titleLink: "https://chalecreta.vercel.app",
+    sourceLink: "https://github.com/Allan-Neves/Creta-Chale",
+    technology: ["HTML", "CSS", "JS"],
+    // description: ""
+  },
+  {
+    imgSrc: "images/relogio-digital.png",
+    imgAlt: "Relógio Digital",
+    title: "Relógio Digital",
+    titleLink: "https://allan-neves.github.io/Relogio-Digital/",
+    sourceLink: "https://github.com/Allan-Neves/Relogio-Digital",
+    technology: ["HTML", "CSS", "JS"],
+    // description: "Veja esta landing page de uma clínica veterinária. Marque um consulta para o seu pet e deixe a saúde dele em dia."
+  },
+  {
+    imgSrc: "images/petlife.png",
+    imgAlt: "PetLife",
+    title: "PetLife",
+    titleLink: "https://allan-neves.github.io/PetLife/",
+    sourceLink: "https://github.com/Allan-Neves/PetLife",
+    technology: ["HTML", "CSS", "JS"],
+    // description: "Veja esta landing page de uma clínica veterinária. Marque um consulta para o seu pet e deixe a saúde dele em dia."
+  },
+  {
+    imgSrc: "images/contador.png",
+    imgAlt: "Contador",
+    title: "Contador",
+    titleLink: "https://allan-neves.github.io/Contador/",
+    sourceLink: "https://github.com/Allan-Neves/Contador",
+    technology: ["HTML", "CSS", "JS"],
+    // description: "Um contador simples, funcional e prático."
+  },
+  {
+    imgSrc: "images/regra-de-tres.png",
+    imgAlt: "Regra de Três",
+    title: "Regra de Três",
+    titleLink: "https://allan-neves.github.io/Regra-de-tres/",
+    sourceLink: "https://github.com/Allan-Neves/Regra-de-tres",
+    technology: ["HTML", "CSS", "JS"],
+    // description: "Este projeto foi feito com o auxilio de um tutorial. Agora podemos fazer nossas regras de três de forma rápida e prática."
+  },
+  {
     imgSrc: "images/madame-chocoberry.png",
     imgAlt: "Madame Chocoberry",
     title: "Madame Chocoberry",
     titleLink: "https://madamechocoberry.vercel.app/",
     sourceLink: "https://github.com/Allan-Neves/Madame-Chocoberry",
     technology: ["HTML", "CSS", "JS"],
-    description: "Projeto feito puramente por mim, aprecie as delícias feitas pela Madame Chocoberry!"
+    // description: "Projeto feito puramente por mim, aprecie as delícias feitas pela Madame Chocoberry!"
   },
   {
     imgSrc: "images/top-links.png",
@@ -15,7 +60,7 @@ var cardData = [
     titleLink: "https://toplinks.vercel.app/",
     sourceLink: "https://github.com/Allan-Neves/Top-links",
     technology: ["HTML", "CSS"],
-    description: "Este projeto conta com a ajuda de diversas pessoas. Nele você encontrará links que irão te auxiliar nas mais diversas atividades."
+    // description: "Este projeto conta com a ajuda de diversas pessoas. Nele você encontrará links que irão te auxiliar nas mais diversas atividades."
   },
   {
     imgSrc: "images/social-links.png",
@@ -24,7 +69,7 @@ var cardData = [
     titleLink: "https://allan-carlos.vercel.app",
     sourceLink: "https://github.com/Allan-Neves/Social-Links",
     technology: ["HTML", "CSS"],
-    description: "Encontre aqui todas as minhas redes sociais! Um site com um design simples e bonito, você deveria dar uma espiada."
+    // description: "Encontre aqui todas as minhas redes sociais! Um site com um design simples e bonito, você deveria dar uma espiada."
   },
   {
     imgSrc: "images/churrascometro.png",
@@ -33,7 +78,7 @@ var cardData = [
     titleLink: "https://allan-carlos.github.io/Churrascometro/",
     sourceLink: "https://github.com/Allan-Neves/Churrascometro",
     technology: ["HTML", "CSS", "JS"],
-    description: "Está planejando um churrasco e não tem certeza de quantos itens você precisa? Não se preocupe, você está no lugar certo! Aqui, podemos ajudá-lo a calcular a quantidade ideal de itens para garantir que seu churrasco seja um sucesso."
+    // description: "Está planejando um churrasco e não tem certeza de quantos itens você precisa? Não se preocupe, você está no lugar certo! Aqui, podemos ajudá-lo a calcular a quantidade ideal de itens para garantir que seu churrasco seja um sucesso."
   },
   {
     imgSrc: "images/workshop.png",
@@ -42,7 +87,7 @@ var cardData = [
     titleLink: "https://allan-carlos.github.io/Workshop/",
     sourceLink: "https://github.com/Allan-Neves/Workshop",
     technology: ["HTML", "CSS"],
-    description: "Não perca a chance de participar do nosso workshop! Inscreva-se facilmente através da nossa página de captura responsiva, com integração ao Mailchimp. Você vai adorar o resultado final!"
+    // description: "Não perca a chance de participar do nosso workshop! Inscreva-se facilmente através da nossa página de captura responsiva, com integração ao Mailchimp. Você vai adorar o resultado final!"
   },
   {
     imgSrc: "images/cordel.png",
@@ -51,7 +96,7 @@ var cardData = [
     titleLink: "https://allan-carlos.github.io/Cordel/",
     sourceLink: "https://github.com/Allan-Neves/Cordel",
     technology: ["HTML", "CSS"],
-    description: "Mergulhe na magia do Cordel Moderno com um toque de tecnologia. Inspirado pelo Curso em Vídeo e pelos textos geniais de Milton Duarte, este site combina cultura e inovação em uma experiência dinâmica e cativante."
+    // description: "Mergulhe na magia do Cordel Moderno com um toque de tecnologia. Inspirado pelo Curso em Vídeo e pelos textos geniais de Milton Duarte, este site combina cultura e inovação em uma experiência dinâmica e cativante."
   },
   {
     imgSrc: "images/pagina-de-conteudo.png",
@@ -60,7 +105,7 @@ var cardData = [
     titleLink: "https://allan-carlos.github.io/Pagina-de-conteudo/",
     sourceLink: "https://github.com/Allan-Neves/Pagina-de-conteudo",
     technology: ["HTML", "CSS"],
-    description: "Conheça os ‘vilões’ da luz em uma página de conteúdo super interessante. Escrito por Paulo César de Andrade e publicado no site ‘Qual compra?’, este artigo oferece dicas valiosas sobre economia de energia. Você vai se surpreender!"
+    // description: "Conheça os ‘vilões’ da luz em uma página de conteúdo super interessante. Escrito por Paulo César de Andrade e publicado no site ‘Qual compra?’, este artigo oferece dicas valiosas sobre economia de energia. Você vai se surpreender!"
   },
   {
     imgSrc: "images/reencontrow.png",
@@ -69,7 +114,7 @@ var cardData = [
     titleLink: "https://allan-carlos.github.io/Reencontrow/",
     sourceLink: "https://github.com/Allan-Neves/Reencontrow",
     technology: ["HTML", "CSS"],
-    description: "Encontre o seu par perfeito na comunidade apaixonante no Reencontrow. Com mais de 4.000 membros, discutimos relacionamentos, desenvolvimento pessoal e música. Junte-se a nós e encontre o seu amor aqui!"
+    // description: "Encontre o seu par perfeito na comunidade apaixonante no Reencontrow. Com mais de 4.000 membros, discutimos relacionamentos, desenvolvimento pessoal e música. Junte-se a nós e encontre o seu amor aqui!"
   },
   {
     imgSrc: "images/robo-android.png",
@@ -78,7 +123,7 @@ var cardData = [
     titleLink: "https://allan-carlos.github.io/Robo-Android/",
     sourceLink: "https://github.com/Allan-Neves/Robo-Android",
     technology: ["HTML", "CSS"],
-    description: "Descubra a história do mascote do Android em um site repleto de curiosidades tecnológicas. Com um design responsivo e elementos divertidos, mergulhe na emoção da tecnologia. Venha conhecer no link acima!"
+    // description: "Descubra a história do mascote do Android em um site repleto de curiosidades tecnológicas. Com um design responsivo e elementos divertidos, mergulhe na emoção da tecnologia. Venha conhecer no link acima!"
   },
   {
     imgSrc: "images/academia-infinity.png",
@@ -87,7 +132,7 @@ var cardData = [
     titleLink: "https://allan-carlos.github.io/Academia-Infinity/",
     sourceLink: "https://github.com/Allan-Neves/Academia-Infinity",
     technology: ["HTML", "CSS"],
-    description: "Prepare-se para uma experiência de tirar o fôlego na Academia Infinity. Com botões interativos e belos efeitos, descubra a magia dos cliques, venha em busca do shape dos Deuses!"
+    // description: "Prepare-se para uma experiência de tirar o fôlego na Academia Infinity. Com botões interativos e belos efeitos, descubra a magia dos cliques, venha em busca do shape dos Deuses!"
   },
   {
     imgSrc: "images/balle-bot.png",
@@ -96,7 +141,7 @@ var cardData = [
     titleLink: "https://allan-carlos.github.io/Balle-Bot/",
     sourceLink: "https://github.com/Allan-Neves/Balle-Bot",
     technology: ["HTML", "CSS"],
-    description: "Conheça a incrível landing page da Balle Bot, uma bot de Discord charmosa. Com um design elegante, esta página é o lugar perfeito para aprender mais sobre a bot e adicioná-la à sua comunidade."
+    // description: "Conheça a incrível landing page da Balle Bot, uma bot de Discord charmosa. Com um design elegante, esta página é o lugar perfeito para aprender mais sobre a bot e adicioná-la à sua comunidade."
   },
   {
     imgSrc: "images/parallax.png",
@@ -105,7 +150,7 @@ var cardData = [
     titleLink: "https://allan-carlos.github.io/Parallax-Website/",
     sourceLink: "https://github.com/Allan-Neves/Parallax-Website",
     technology: ["HTML", "CSS"],
-    description: "Prepare-se para se divertir com o mágico efeito Parallax em todas as imagens deste site. Navegue pela cidade e desfrute da dança mágica das imagens. Não perca a chance de conferir!"
+    // description: "Prepare-se para se divertir com o mágico efeito Parallax em todas as imagens deste site. Navegue pela cidade e desfrute da dança mágica das imagens. Não perca a chance de conferir!"
   },
   {
     imgSrc: "images/fullscreen-landing.png",
@@ -114,7 +159,7 @@ var cardData = [
     titleLink: "https://allan-carlos.github.io/Fullscreen-Landing/",
     sourceLink: "https://github.com/Allan-Neves/Fullscreen-Landing",
     technology: ["HTML", "CSS"],
-    description: "Seja bem-vindo à nossa landing-page mágica! Inspirada no template do site jolly-kalam-23776e.netlify.app, esta página deslumbrante utiliza flex-box para um layout simples e eficaz."
+    // description: "Seja bem-vindo à nossa landing-page mágica! Inspirada no template do site jolly-kalam-23776e.netlify.app, esta página deslumbrante utiliza flex-box para um layout simples e eficaz."
   },
   {
     imgSrc: "images/pagina-de-receitas.png",
@@ -123,7 +168,7 @@ var cardData = [
     titleLink: "https://allan-carlos.github.io/Pagina-de-receitas/",
     sourceLink: "https://github.com/Allan-Neves/Pagina-de-receitas",
     technology: ["HTML", "CSS"],
-    description: "Aprenda a fazer um delicioso Bolo de Fubá com Goiabada nessa página de receita do TudoGostoso. Com uma foto tentadora e instruções fáceis de seguir, você vai se deliciar com o resultado final. Venha experimentar!"
+    // description: "Aprenda a fazer um delicioso Bolo de Fubá com Goiabada nessa página de receita do TudoGostoso. Com uma foto tentadora e instruções fáceis de seguir, você vai se deliciar com o resultado final. Venha experimentar!"
   },
   {
     imgSrc: "images/corps.png",
@@ -132,7 +177,7 @@ var cardData = [
     titleLink: "https://allan-carlos.github.io/Corps/",
     sourceLink: "https://github.com/Allan-Neves/Corps",
     technology: ["HTML", "CSS"],
-    description: "Inspirado pelo template do jolly-kalam-23776e.netlify.app É um site completo e com um design adaptável a qualquer dispositivo, explore todas as funcionalidades e beleza deste site."
+    // description: "Inspirado pelo template do jolly-kalam-23776e.netlify.app É um site completo e com um design adaptável a qualquer dispositivo, explore todas as funcionalidades e beleza deste site."
   },
   {
     imgSrc: "images/photography.png",
@@ -141,6 +186,6 @@ var cardData = [
     titleLink: "https://allan-carlos.github.io/Photography/",
     sourceLink: "https://github.com/Allan-Neves/Photography",
     technology: ["HTML", "CSS"],
-    description: "Prepare-se para se apaixonar pela elegância e funcionalidade do design da ACME PHOTOGRAPHY. Com belas fotos naturais e um layout simples, descubra como a simplicidade pode ser verdadeiramente mágica."
+    // description: "Prepare-se para se apaixonar pela elegância e funcionalidade do design da ACME PHOTOGRAPHY. Com belas fotos naturais e um layout simples, descubra como a simplicidade pode ser verdadeiramente mágica."
   }
 ];
